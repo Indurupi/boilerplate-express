7947bd9c7a37848ae7fa4474786718037c3e0e28
Indu rupi <indu795@gmail.com>
2021-05-11 15:26:50 +0530

    fetch data from request param
    https://www.freecodecamp.org/learn/apis-and-microservices/basic-node-and-express/get-route-parameter-input-from-the-client

--- myApp.js
+++ myApp.js
@@ -1,20 +1,24 @@
 var express = require('express');
 var app = express();
 
-const absolutePath = __dirname + "/views/index.html"
 
 app.use(express.static(__dirname));
 
-// it will be called before every request
+// this middleware will be called before every request
 app.use(function logger(req, res, next) {
   console.log(req.method+" "+ req.path +" - "+ req.ip);
   next();
 })
 
+
+const homePagePath = __dirname + "/views/index.html"
+
+// sending index file to home page
 app.get("/", function(req, res) {
-  res.sendFile(absolutePath);
+  res.sendFile(homePagePath);
 })
 
+// using env variable
 app.get("/json", function(req, res) {
   let response = "Hello json";
   if(process.env.MESSAGE_STYLE == "uppercase") {
@@ -23,11 +27,18 @@ app.get("/json", function(req, res) {
   res.json({"message": response });
 })
 
-app.get("/now", function(req, res, next) {
+// attach a middleware function to a get/post call
+app.get("/now", function(req, res, next) { // this is a middleware function
   req.time = new Date().toString();
   next();
-}, function(req, res) {
+}, function(req, res) { // this is final handler
   res.json({time: req.time})
 })
+
+// fetch data from request param
+app.get("/:word/echo", function(req, res) {
+  res.json({ echo: req.params.word })
+})
+
 
 module.exports = app;
